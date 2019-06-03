394a7227664083594f3339e8b0375d885aea3c27
BentoBellone <telliez_thibault@orange.fr>
2019-06-03 18:46:41 +0200

    [thib] added database : favorites + apisearches

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'apisearches#index'
+  resources :apisearches, only: [:index, :show, :new, :create, :destroy]
+  resources :favorites, only: [:index, :show, :new, :create, :destroy]
 end
