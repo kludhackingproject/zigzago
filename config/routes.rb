ffc3024297a280e7bdeb903f8f34cc68a1833a52
klud <vincentgillibert@gmail.com>
2019-06-05 01:02:51 +0200

    [Vincent] Correct changes from Amadeus API

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'apisearches#index', as: 'home'
+  root 'apisearches#index'
   resources :apisearches, only: [:index, :show, :new, :create, :destroy]
   resources :favorites, only: [:index, :show, :new, :create, :destroy]
   resources :profiles, only: [:show]
