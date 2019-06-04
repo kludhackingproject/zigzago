28ca11a4a078533079a4011f745ccc8e5a338345
klud <vincentgillibert@gmail.com>
2019-06-04 12:59:53 +0200

    [vincent] Implement a search form / to with ITA code

--- app/controllers/apisearches_controller.rb
+++ app/controllers/apisearches_controller.rb
@@ -1,23 +1,45 @@
 class ApisearchesController < ApplicationController
     def index
-      amadeus = Amadeus::Client.new
+      # Airport & City Search (autocomplete)
+      # Find all the cities and airports starting by the keyword 'LON'
 
-      response = amadeus.shopping.flight_offers.get(
-        origin: 'NYC',
-        destination: 'MAD',
-        departureDate: '2019-10-01'
-      )
-
-      @amadeus_test = response.data
     end
 
     def show
+
+
+
     end
 
     def new
     end
 
     def create
+
+      amadeus = Amadeus::Client.new
+
+      response = amadeus.shopping.flight_offers.get(
+        origin: params[:departure],
+        destination: params[:arrival],
+        departureDate: '2019-10-01'
+      )
+
+      @results = response.result
+
+      # amadeus = Amadeus::Client.new
+      #
+      # response =  amadeus.reference_data.locations.get(
+      #   keyword: params[:keyword],
+      #   subType: Amadeus::Location::ANY
+      # )
+      #
+      # @results = response.result
+
+      respond_to do |format|
+        format.html { redirect_to apisearches_path }
+        format.js { }
+      end
+
     end
 
     def destroy
