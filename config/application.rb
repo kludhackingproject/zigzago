d69f5555f991dcbfb134780a759e46e45edbe671
Archipo <aj.antoinejacques@gmail.com>
2019-06-05 18:05:06 +0200

    heroku

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,3 @@
-config.assets.initialize_on_precompile = false
 require_relative 'boot'
 
 require 'rails/all'
@@ -11,7 +10,7 @@ module SomewhereToGo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
