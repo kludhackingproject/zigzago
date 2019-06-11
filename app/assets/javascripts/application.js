32c01591c23d5ab1a054f62caeed95fa6e62367e
Archipo <aj.antoinejacques@gmail.com>
2019-06-11 14:43:13 +0200

    fixing heroku assets failed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -74,7 +74,7 @@ $(document).ready(function() {
 
 ///// Tawk /////
 
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+var Tawk_API=Tawk_API||{}; Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
