50d9e9c69cdc1838beb891c5ec43f341fd0289a2
Archipo <aj.antoinejacques@gmail.com>
2019-06-11 15:18:24 +0200

    fixing heroku assets failed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -72,3 +72,15 @@ $(document).ready(function() {
   	});
   });
 
+  
+///// Tawk /////
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5cff5ce3267b2e578531c6f8/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
